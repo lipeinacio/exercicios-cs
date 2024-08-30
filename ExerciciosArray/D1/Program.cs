49c0a9c99316587ebede4328bb2f067341db839c
Felipe Inacio <felipeinacio.contato@gmail.com>
2024-08-30 14:43:19 -0300

    exercícios cs

---
+++ ExerciciosArray/D1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Exercício 1: 
+Avaliação de Notas de Estudantes
+Descrição: Crie um programa que leia as notas de 6 estudantes em uma disciplina. Armazene essas notas em um array e calcule a nota total e a média das notas. Em seguida, informe a nota mais alta e a mais baixa da turma.
+    Dicas:
+Use um loop para ler as notas dos estudantes e armazená-las no array.
+Para calcular a soma e a média, você pode usar um acumulador e a contagem de elementos.
+Para encontrar a nota mais alta e a mais baixa, você pode comparar os valores enquanto lê as notas.
+*/
+
+int[] notas = new int[6];
+int soma = 0, notaMenor = int.MaxValue, notaMaior = int.MinValue;
+
+for (int i = 0; i< notas.Length; i++)
+{
+    Console.WriteLine($"Digite a nota do {i+1}° Aluno ");
+    notas[i] = int.Parse(Console.ReadLine());
+    soma += notas[i];
+
+    if (notas[i] > notaMaior)
+    {
+        notaMaior =  notas[i];
+    }
+    if (notas[i] < notaMenor)
+    {
+        notaMenor = notas[i];
+    }
+}
+    Console.WriteLine($"\nTotal de notas: {soma}");
+    Console.WriteLine($"Media das notas: {soma/notas.Length:F2}");
+    Console.WriteLine($"Maior Nota: {notaMaior}");
+    Console.WriteLine($"Menor Nota: {notaMenor}");
---
+++ ExerciciosArray/D2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Exercício 2: 
+Sequência de Quadrados
+Descrição: Crie um programa que preencha um array com os quadrados dos números inteiros de 1 a 10. Exiba o conteúdo do array e depois mostre a soma de todos os quadrados.
+    Dicas:
+Use um loop para preencher o array com os quadrados dos números de 1 a 10.
+Depois, use outro loop ou um método para calcular a soma dos valores no array.
+*/
+
+int[] quadrados = new int[10];
+int soma = 0;
+
+for (int i = 0; i < quadrados.Length; i++) 
+{
+    quadrados[i] = (i + 1) * (i + 1);
+    soma += quadrados[i];
+    Console.WriteLine($"Posição {i + 1}°: {quadrados[i]}");
+}
+
+Console.WriteLine($"\nSoma de todos os quadrados: {soma}");
